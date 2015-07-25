7ee0f5ad5d0108bf0d1eeaa5dec7d5467752ef4f
Sergio Sánchez Ortiz <sersanor@gmail.com>
2015-07-26 01:55:25 +0200

    vis downloads ASSETs + red Info Icons

--- Assets/ui/mainUI.cs
+++ Assets/ui/mainUI.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using Vuforia;
 using SimpleJSON;
 
-public class mainUI : MonoBehaviour {
+public class mainUI : MonoBehaviour
+{
 
-
+	//GLOBAL CONST START
+	public static JSONNode element;
+	public static string elementName;
+	//GLOBAL CONST END
 	private Rect box; //UI BOX RECT
 	private GUIStyle mStyle; //BACKGROUND IMAGE
 	//--//
@@ -15,7 +19,7 @@ public class mainUI : MonoBehaviour {
 	GUIStyle mOKButtonBgStyle;
 	private const float ABOUT_TEXT_MARGIN = 20.0f;
 	private const float START_BUTTON_VERTICAL_MARGIN = 10.0f;
-	private const string mTitle="APP Realidad Aumentada Valencia";
+	private const string mTitle = "APP Realidad Aumentada Valencia";
 	private float mStartButtonAreaHeight = 80.0f;
 	private GUISkin mUISkin;
 	private Vector2 mScrollPosition;
@@ -29,28 +33,29 @@ public class mainUI : MonoBehaviour {
 	private static JSONNode J;
 	private static JSONNode aux;
 
-	private static float DeviceDependentScale
-	{
-		get { if ( Screen.width > Screen.height)
-			return Screen.height / 480f;
+	private static float DeviceDependentScale {
+		get {
+			if (Screen.width > Screen.height)
+				return Screen.height / 480f;
 			else 
 				return Screen.width / 480f; 
 		}
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
 		//CALL CAMERA
-		m_AboutText = Resources.Load("PFC_About") as TextAsset;
-		mAboutTitleBgStyle = new GUIStyle();
-		mOKButtonBgStyle = new GUIStyle();
+		m_AboutText = Resources.Load ("PFC_About") as TextAsset;
+		mAboutTitleBgStyle = new GUIStyle ();
+		mOKButtonBgStyle = new GUIStyle ();
 		mAboutTitleBgStyle.normal.background = Resources.Load ("Images/grayTexture") as Texture2D;
 		mOKButtonBgStyle.normal.background = Resources.Load ("Images/capture_button_normal_XHigh") as Texture2D;
-		mAboutTitleBgStyle.font = Resources.Load("SourceSansPro-Regular_big_xhdpi") as Font;
-		mOKButtonBgStyle.font = Resources.Load("SourceSansPro-Regular_big_xhdpi") as Font;
-		mUISkin = Resources.Load("Images/ButtonSkinsXHDPI") as GUISkin;
-		mUISkin.label.font = Resources.Load("SourceSansPro-Regular") as Font;
+		mAboutTitleBgStyle.font = Resources.Load ("SourceSansPro-Regular_big_xhdpi") as Font;
+		mOKButtonBgStyle.font = Resources.Load ("SourceSansPro-Regular_big_xhdpi") as Font;
+		mUISkin = Resources.Load ("Images/ButtonSkinsXHDPI") as GUISkin;
+		mUISkin.label.font = Resources.Load ("SourceSansPro-Regular") as Font;
 		mOKButtonBgStyle.normal.textColor = Color.white;
 		mAboutTitleBgStyle.alignment = TextAnchor.MiddleLeft;
 		mOKButtonBgStyle.alignment = TextAnchor.MiddleCenter;
@@ -69,9 +74,11 @@ public class mainUI : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		timer -= 1;
-		if(timer==0)callAutoFocus ();
+		if (timer == 0)
+			callAutoFocus ();
 
 		if (Application.platform == RuntimePlatform.WindowsEditor) {
 			checkInputWindows ();
@@ -80,120 +87,142 @@ public class mainUI : MonoBehaviour {
 		}
 	}
 
-	void callAutoFocus(){
+	void callAutoFocus ()
+	{
 		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
 	}
 
-	void OnGUI (){
+	void OnGUI ()
+	{
 		if (mustDraw) {
 			draw (mTitle);
 		}
 		if (objectTapped) {
-			draw (aux["nombre"],aux["descCompleta"],"","",aux["coords"],"","Cerrar",aux["coords"]);
+			draw (aux ["nombre"], aux ["descCompleta"], "", aux ["coords"], aux ["modelo3d"], "Cerrar", aux ["coords"]);
 		}
 	}
 
-	void OnApplicationPause(bool pause)
+	void OnApplicationPause (bool pause)
 	{
-		if (!pause)
-		{
+		if (!pause) {
 			// App resumed
 			callAutoFocus ();
 		}
 	}
 
 	//DRAW ABOUT PAGE
-	void drawAbout (){
+	void drawAbout ()
+	{
 
 		//DRAW THE BOX
-		box = new Rect(0, 0, Screen.width, Screen.height);
-		mStyle = new GUIStyle();
-		mStyle.normal.background = Resources.Load("Images/main_background") as Texture2D;
-		GUI.Box(box, "", mStyle);
+		box = new Rect (0, 0, Screen.width, Screen.height);
+		mStyle = new GUIStyle ();
+		mStyle.normal.background = Resources.Load ("Images/main_background") as Texture2D;
+		GUI.Box (box, "", mStyle);
 	}
 
-	void draw(string title, string desc="about", string img="", string modelo3d="", string coords="", string model3d="", string button="OK", string maps=""){
-		float scale = 1*DeviceDependentScale;
-		mAboutTitleHeight = 80.0f* scale;
-		drawAbout();
-		GUI.Box(new Rect(0,0,Screen.width,mAboutTitleHeight),string.Empty,mAboutTitleBgStyle);
-		GUI.Box(new Rect(ABOUT_TEXT_MARGIN * DeviceDependentScale,0,Screen.width,mAboutTitleHeight),title,mAboutTitleBgStyle);
+	void draw (string title, string desc="about", string img="", string coords="", string model3d="", string button="OK", string maps="")
+	{
+		float scale = 1 * DeviceDependentScale;
+		mAboutTitleHeight = 80.0f * scale;
+		drawAbout ();
+		GUI.Box (new Rect (0, 0, Screen.width, mAboutTitleHeight), string.Empty, mAboutTitleBgStyle);
+		GUI.Box (new Rect (ABOUT_TEXT_MARGIN * DeviceDependentScale, 0, Screen.width, mAboutTitleHeight), title, mAboutTitleBgStyle);
 		float width = Screen.width / 1.5f;
 		//float height = startButtonStyle.normal.background.height * scale;
 		float height = mOKButtonBgStyle.normal.background.height * scale;
 		
-		mStartButtonAreaHeight = height + 2*(START_BUTTON_VERTICAL_MARGIN * scale);
-		float left = Screen.width/2 - width/2;
+		mStartButtonAreaHeight = height + 2 * (START_BUTTON_VERTICAL_MARGIN * scale);
+		float left = Screen.width / 2 - width / 2;
 		float top = Screen.height - mStartButtonAreaHeight + START_BUTTON_VERTICAL_MARGIN * scale;
 		
 		GUI.skin = mUISkin;
 		
-		GUILayout.BeginArea(new Rect(ABOUT_TEXT_MARGIN * DeviceDependentScale,
+		GUILayout.BeginArea (new Rect (ABOUT_TEXT_MARGIN * DeviceDependentScale,
 		                             mAboutTitleHeight + 5 * DeviceDependentScale,
 		                             Screen.width - (ABOUT_TEXT_MARGIN * DeviceDependentScale),
-		                             Screen.height - ( mStartButtonAreaHeight) - mAboutTitleHeight - 5 * DeviceDependentScale));
+		                             Screen.height - (mStartButtonAreaHeight) - mAboutTitleHeight - 5 * DeviceDependentScale));
 		
-		mScrollPosition = GUILayout.BeginScrollView(mScrollPosition, false, false, GUILayout.Width(Screen.width - (ABOUT_TEXT_MARGIN * DeviceDependentScale)), 
+		mScrollPosition = GUILayout.BeginScrollView (mScrollPosition, false, false, GUILayout.Width (Screen.width - (ABOUT_TEXT_MARGIN * DeviceDependentScale)), 
 		                                            GUILayout.Height (Screen.height - mStartButtonAreaHeight - mAboutTitleHeight));
 		
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace ();
 
-		if(desc == "about")
-			GUILayout.Label(m_AboutText.text);
+		if (desc == "about")
+			GUILayout.Label (m_AboutText.text);
 		else
-			GUILayout.Label(desc);
+			GUILayout.Label (desc);
 		
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndHorizontal ();
 		
-		GUILayout.EndScrollView();
+		GUILayout.EndScrollView ();
 		
-		GUILayout.EndArea();
-		if(model3d!="")
-		if (GUI.Button(new Rect(left/2, top, width/3, height), "3D Scene" ,mOKButtonBgStyle))
-		{
-			//Draw new scene
-		}
-		// if button was pressed, remember to make sure this event is not interpreted as a touch event somewhere else
-		if (GUI.Button(new Rect(left+width/3, top, width/3, height), button ,mOKButtonBgStyle))
-		{
-			mustDraw = objectTapped = false;
-			//update the vector pos
-			mScrollPosition = new Vector2();
-		}
-		if(maps!="")
-		if (GUI.Button(new Rect(left+left/2+width*2/3, top, width/3, height), "MAPS" ,mOKButtonBgStyle))
-		{
-			Application.OpenURL(maps);
+		GUILayout.EndArea ();
+		if (model3d != "") { // 3 buttons
+			if (GUI.Button (new Rect (left / 2, top, width / 3, height), "3D Scene", mOKButtonBgStyle)) {
+			
+				element = aux; // set the global variable for acces from the other scene
+				Application.LoadLevel ("vis");	//Draw new scene
+
+			}
+			// if button was pressed, remember to make sure this event is not interpreted as a touch event somewhere else
+			if (GUI.Button (new Rect (left + width / 3, top, width / 3, height), button, mOKButtonBgStyle)) {
+				mustDraw = objectTapped = false;
+				//update the vector pos
+				mScrollPosition = new Vector2 ();
+			}
+			if (maps != "")
+			if (GUI.Button (new Rect (left + left / 2 + width * 2 / 3, top, width / 3, height), "MAPS", mOKButtonBgStyle)) {
+				Application.OpenURL (maps);
+			}
+		} else if (maps != "") { // ONLY 2 BUTTONS
+			if (GUI.Button (new Rect (left, top, width / 3, height), button, mOKButtonBgStyle)) {
+				mustDraw = objectTapped = false;
+				//update the vector pos
+				mScrollPosition = new Vector2 ();
+			}
+			if (GUI.Button (new Rect (2 * left + width / 3, top, width / 3, height), "MAPS", mOKButtonBgStyle)) {
+				Application.OpenURL (maps);
+			}
+		} else { // JUST 1 BUTTON
+			if (GUI.Button (new Rect (left + width / 3, top, width / 3, height), button, mOKButtonBgStyle)) {
+				mustDraw = objectTapped = false;
+				//update the vector pos
+				mScrollPosition = new Vector2 ();
+				//Application.LoadLevel("vis");
+			}
 		}
 	} // END DRAW
 
-	void checkInputMobile(){
+	void checkInputMobile ()
+	{
 		
-		for(int i = 0; i < Input.touchCount; ++i)
-		if (Input.GetTouch(i).phase.Equals(TouchPhase.Began)) {
-			{
-				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-				if (Physics.Raycast(ray, out hit)) {
-					hit.transform.gameObject.SendMessage("OnMouseDown");
-					aux = J["pdi"][hit.transform.gameObject.name];
-					objectTapped = true;
+		for (int i = 0; i < Input.touchCount; ++i)
+			if (Input.GetTouch (i).phase.Equals (TouchPhase.Began)) {
+				{
+					Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (i).position);
+					if (Physics.Raycast (ray, out hit)) {
+						hit.transform.gameObject.SendMessage ("OnMouseDown");
+						aux = J ["pdi"] [hit.transform.gameObject.name];
+						elementName = hit.transform.gameObject.name;
+						objectTapped = true;
+					}
 				}
-			}
 			
-		}
+			}
 	}
 	
-	void checkInputWindows(){
-		if (Input.GetKeyDown (KeyCode.Mouse0))
-		{
+	void checkInputWindows ()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			RaycastHit raycastHit;
 			Ray ray = Camera.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out raycastHit, 1000.0f) && raycastHit.collider)
-			{
-				Debug.Log (raycastHit.transform.name + "Hitted");
-				aux = J["pdi"][raycastHit.transform.name];
+			if (Physics.Raycast (ray, out raycastHit, 1000.0f) && raycastHit.collider) {
+				Debug.Log (raycastHit.transform.name + " Hitted");
+				aux = J ["pdi"] [raycastHit.transform.name];
+				elementName = raycastHit.transform.name;
 				objectTapped = true;
 
 			}
@@ -201,26 +230,23 @@ public class mainUI : MonoBehaviour {
 		
 	}
 
-	IEnumerator CheckConnection()
+	IEnumerator CheckConnection ()
 	{
 		const float timeout = 10f;
 		float startTime = Time.timeSinceLevelLoad;
-		Ping ping = new Ping("google.com");
+		Ping ping = new Ping ("google.com");
 		
-		while (true)
-		{
-			if (ping.isDone)
-			{
+		while (true) {
+			if (ping.isDone) {
 				inet = true;
 				yield break;
 			}
-			if (Time.timeSinceLevelLoad - startTime > timeout)
-			{
+			if (Time.timeSinceLevelLoad - startTime > timeout) {
 				inet = false;
 				yield break;
 			}
 			
-			yield return new WaitForEndOfFrame();
+			yield return new WaitForEndOfFrame ();
 		}
 	}
 	IEnumerator downloadJson ()
---
+++ Assets/ui/visUI.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class visUI : MonoBehaviour
+{
+	
+	public float minSwipeDistY;
+	public float minSwipeDistX;
+	public GameObject objeto;
+	public float speedRotation = 1.0f;
+	private Vector2 startPos;
+	private GameObject mBundleInstance;
+	private string AssetName;
+	public Camera mainCamera;
+	private JSONNode aux;
+
+	void Start ()
+	{
+
+		aux = mainUI.element;
+		if (aux != null) {
+			Debug.Log ("Info Token");
+			AssetName = mainUI.elementName;
+			Debug.Log (AssetName);
+			StartCoroutine (Download ());
+		} else
+			Debug.Log ("No Info");
+	}
+
+	void Update ()
+	{
+		touchAndroid ();
+		//rotate ();
+	}
+
+	// START DOWLOAD ASSETS
+	IEnumerator Download ()
+	{
+		string bundleURL = aux ["modelo3d"];
+			
+		Debug.Log (bundleURL);
+		using (WWW www = WWW .LoadFromCacheOrDownload(bundleURL, 1)) {
+			yield return www;
+				
+			if (www .error != null)
+				throw new UnityException ("WWW Download had an error: " + www .error);
+				
+			AssetBundle bundle = www .assetBundle;
+			if (AssetName == "CValencia") {
+				Debug.Log ("Drawing");
+				mBundleInstance = Instantiate (bundle.mainAsset) as GameObject;
+				mBundleInstance.transform.localPosition = new Vector3 (0, 0, 0);
+				mainCamera.transform.position = new Vector3 (0, 0.2F, -1);
+				mainCamera.transform.LookAt (mBundleInstance.transform.position);
+
+			} else {
+				Debug.Log ("NOT BUNDLE");
+				mBundleInstance = Instantiate (bundle.LoadAsset (AssetName)) as GameObject;
+			}
+		}
+	}
+	// END DOWNLOAD ASSETS
+
+
+	void rotate (string pos)
+	{
+		if (pos == "up")
+			mBundleInstance.transform.Rotate (Vector3.up * Time.deltaTime * speedRotation);
+		if (pos == "down")
+			mBundleInstance.transform.Rotate (Vector3.down * Time.deltaTime * speedRotation);
+		if (pos == "left")
+			mBundleInstance.transform.Rotate (Vector3.up * Time.deltaTime * speedRotation);
+		if (pos == "right") {
+			mBundleInstance.transform.Rotate (Vector3.down * Time.deltaTime * speedRotation);
+		}
+	}
+
+
+	void touchAndroid ()
+	{
+		{
+			//#if UNITY_ANDROID
+			if (Input.touchCount > 0) {
+				Touch touch = Input.touches [0];
+				switch (touch.phase) {
+					
+				case TouchPhase.Began:
+					startPos = touch.position;
+
+					break;
+
+				case TouchPhase.Moved:
+					float swipeDistHorizontal = (new Vector3 (touch.position.x, 0, 0) - new Vector3 (startPos.x, 0, 0)).magnitude;
+					
+					if (swipeDistHorizontal > minSwipeDistX) {
+						
+						float swipeValue = Mathf.Sign (touch.position.x - startPos.x);
+						
+						if (swipeValue > 0) {//right swipe
+							rotate ("right");
+						} else if (swipeValue < 0) {//left swipe
+							rotate ("left");
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+}
